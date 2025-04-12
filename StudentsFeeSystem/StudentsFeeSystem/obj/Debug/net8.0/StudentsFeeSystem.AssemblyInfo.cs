0ef2080dbd7aa5a14cabc89e42348a75d63359e7
jobayer-alam-24 <alamsheikhjobayer2000@gmail.com>
2025-04-12 23:54:08 +0600

    done

--- StudentsFeeSystem/StudentsFeeSystem/obj/Debug/net8.0/StudentsFeeSystem.AssemblyInfo.cs
+++ StudentsFeeSystem/StudentsFeeSystem/obj/Debug/net8.0/StudentsFeeSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentsFeeSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db915cdd8dbdc7943f3557b75a5ea257c8bb445")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfcd27d5b0ed7a93103188d7115dfa36b6c492a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentsFeeSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentsFeeSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
